94d5bef5bfce19b909b68b449ffa5d27987b9ad0
b06202019 <31923715+b06202019@users.noreply.github.com>
2017-10-11 21:30:45 +0800

    i

--- server.js
+++ server.js
@@ -13,15 +13,15 @@ var file_content;
 
 var webPath = 'public';
 
-var server = http.createServer(function (req, res) {
+var server = http.createServer(function(req, res) {
     // req 是 request 本地端請求的訊息
     // res 是 response 主機回傳到本地端的訊息
 
     // 解析使用者要求的路徑名稱
     let url_path = url.parse(req.url);
-    console.log('path:'+url_path);
+    console.log('path:' + url_path);
     let pathname = url_path.pathname;
-    console.log('pathname:'+pathname);
+    console.log('pathname:' + pathname);
 
     // 判斷pathname是否為預設路徑
     if (pathname === "/" || pathname === "/index.htm") {
@@ -31,22 +31,28 @@ var server = http.createServer(function (req, res) {
     // __dirname 是程式的路徑
     // webPath 是公開的資料夾
     // pathname 是使用者要求的路徑名稱
-    var filePath = path.join(__dirname, webPath, pathname);  
-    console.log('filePath:'+filePath);
+    var filePath = path.join(__dirname, webPath, pathname);
+    console.log('filePath:' + filePath);
+
+    var resHeader = {
+        'Accept-Charset': 'utf-8',
+        'Accept-Language': 'zh-TW',
+        'Content-Type': 'text/html; charset=utf-8',
+    }
 
 
     // 讀取檔案
-    fs.readFile(filePath, 'utf8', function (err, content) {
+    fs.readFile(filePath, 'utf8', function(err, content) {
         if (err) {
             console.log('Failed to read');
             // 若檔案讀取錯誤，回傳 404
-            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.writeHead(404, resHeader);
             res.write('<h1>404. 找不到檔案!!</h1>')
-            res.end();  
+            res.end();
             return;
         }
         // 將檔案內容傳給瀏覽器
-        //res.writeHead(200, { 'Content-Type': 'text/' });
+        res.writeHead(200, resHeader);
         res.write(content);
         res.end();
     })
