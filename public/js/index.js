e945210e9d227ae94693a493cd3943297ffc7a76
b06202019 <31923715+b06202019@users.noreply.github.com>
2017-10-18 21:15:08 +0800

    2115

---
+++ public/js/index.js
@@ -0,0 +1,41 @@
+$('#insert').on('click', function() {
+    var data = {
+        name: $('#InputProductName').val(),
+        price: +$('#InputProductPrice').val(),
+        count: +$('#InputProductCount').val(),
+        image: $('#InputProductImage').val()
+    }
+    $.post("http://127.0.0.1:1377/insert", data, function(response) {
+        if (response) {
+            if (response.result) {
+                $('#message').text('新增成功');
+            } else {
+                $('#message').text('新增失敗 (' + response.data + ')');
+            }
+        } else {
+            $('#message').text('新增失敗');
+        }
+        $('#dialog').modal('show');
+        console.log(response);
+    }, "json");
+})
+
+$('#query').on('click', function() {
+    $.get("http://127.0.0.1:1377/query", function(response) {
+        if (response) {
+            if (response.result) {
+
+                // TODO HW2 作業寫在這裡
+
+
+            } else {
+                $('#message').text('查詢失敗 (' + response.data + ')');
+                $('#dialog').modal('show');
+            }
+        } else {
+            $('#message').text('查詢失敗');
+            $('#dialog').modal('show');
+        }
+        console.log(response);
+    }, "json");
+})
